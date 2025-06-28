5f7f38436010240f8d6b33618a0cd10ec3444093
Victor Cata <victor.cata82@gmail.com>
2025-06-27 21:57:04 -0700

    build(backend): python fastapi setup

---
+++ app-web/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'app-web',
+  preset: '../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../coverage/app-web',
+};
---
+++ app-web/src/app/page.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/message')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch(() => setMessage('Could not fetch message'));
+  }, []);
+
+  return (
+    <main>
+      <h1>Backend Message:</h1>
+      <p>{message}</p>
+    </main>
+  );
+}
